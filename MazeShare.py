de13b102738040b71e627ceb4de0005a23e40c15
SmartPig <59109680+SmartPig-Joe@users.noreply.github.com>
2020-08-27 10:21:48 +0800

    Add files via upload

---
+++ MazeShare.py
@@ -0,0 +1,62 @@
+import requests
+import numpy as np
+import matplotlib.pyplot as plt
+import json
+import os
+import sys
+
+step_url = "https://puzzle.gztime.cc/api/maze/step"
+reset_url = "https://puzzle.gztime.cc/api/maze/reset"
+head = {
+    "cookie": "_ga=GA1.2.153189809.1598319485; _gid=GA1.2.1716356685.1598319485; token=db827d8433ac34efe91731c520018274; Hm_lvt_911fab4aa0da17e3360e19c9727a4684=1598336219,1598337663,1598372622,1598407833; Hm_lpvt_911fab4aa0da17e3360e19c9727a4684=1598441691; _gat_gtag_UA_175500859_1=1"
+}
+
+
+def reset():
+    requests.post(reset_url, headers=head)
+
+
+# status[上下左右]
+def get_status(x):
+    return [int(i) for i in bin(x)[2:].zfill(4)][::-1]
+
+
+def go(x, y, status, bdrc):
+    global t, log, ct
+    ct += 1
+    print(ct, x, y, status)
+    if x == 63 and y == 63: os.system("pause")
+    t[x][y] = status
+    status = get_status(status)
+    if not status[0] and not t[x][y + 1] and ((0 <= x <= 63) and (0 <= y + 1 <= 63)):  # 上
+        r = requests.post(step_url, headers=head, data={'drc': 'n'})
+        # log.append(r)
+        go(x, y + 1, json.loads(r.text)['newedges'], 's')
+    if not status[1] and not t[x][y - 1] and ((0 <= x <= 63) and (0 <= y - 1 <= 63)):  # 下
+        r = requests.post(step_url, headers=head, data={'drc': 's'})
+        # log.append(r)
+        go(x, y - 1, json.loads(r.text)['newedges'], 'n')
+    if not status[2] and not t[x - 1][y] and ((0 <= x - 1 <= 63) and (0 <= y <= 63)):  # 左
+        r = requests.post(step_url, headers=head, data={'drc': 'w'})
+        # log.append(r)
+        go(x - 1, y, json.loads(r.text)['newedges'], 'e')
+    if not status[3] and not t[x + 1][y] and ((0 <= x + 1 <= 63) and (0 <= y <= 63)):  # 右
+        r = requests.post(step_url, headers=head, data={'drc': 'e'})
+        # log.append(r)
+        go(x + 1, y, json.loads(r.text)['newedges'], 'w')
+    requests.post(step_url, headers=head, data={'drc': bdrc})
+
+
+def save():
+    l = [[int(x)] for i in t for x in i]
+    with open("test.json", "w") as f:
+        f.write(json.dumps(l))
+
+
+global t, log, ct
+ct = 0
+sys.setrecursionlimit(1000000)
+log = []
+t = np.zeros((64, 64), dtype=np.int32)
+reset()
+go(0, 0, 6, 0)
